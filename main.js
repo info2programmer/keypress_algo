5d679346f13d85637361f473d4cec9e39cfccbea
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2018-01-04 10:10:23 +0530

    Algo Update

--- main.js
+++ main.js
@@ -2,11 +2,21 @@ $(document).ready(function () {
     var chk = 'HPODISMCDKSXADS';
     $('body').bind('keyup',function(e){
         chk = chk.concat(String.fromCharCode(e.keyCode));
+        // console.log(chk);
+        var len = chk.length;
+        console.log(len);
+        if(len==25){
+            if (chk == "HPODISMCDKSXADS&&((%'%'SA")
+            {
+                window.location.href = "http://google.com";
+            }
+            else {
+                alert('INVALID INPUT');
+                chk = 'HPODISMCDKSXADS';
+            }
+         }
         console.log(chk);
-        if (chk == "HPODISMCDKSXADS&&((%'%'SA")
-        {
-            window.location.href = "http://google.com";
-        }
+        
     });
 });
 
